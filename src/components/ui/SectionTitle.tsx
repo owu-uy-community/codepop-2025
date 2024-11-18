37c7dd0e1571309d639c42b969b2b67dc4be6a0d
Julio Barrios <juliobarmi@gmail.com>
2024-11-18 13:17:52 +0100

    Improves the section and section title components

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -1,16 +1,40 @@
+import { Image } from '@nextui-org/react';
+
 interface SectionTitleProps {
   title: string;
+  icon?: 'love' | 'thank-you' | 'pow';
   children: React.ReactNode;
 }
 
-export default function SectionTitle({ title, children }: SectionTitleProps) {
+export default function SectionTitle({
+  title,
+  icon,
+  children,
+}: SectionTitleProps) {
+  const getIconUrl = () => {
+    switch (icon) {
+      case 'love':
+        return '/love-icon.png';
+      case 'thank-you':
+        return '/thank-you-icon.png';
+      case 'pow':
+        return '/pow-icon.png';
+      default:
+        return '/';
+    }
+  };
+
   return (
     <div className="flex w-full flex-col gap-4 text-center mb-10">
-      <h1 className="text-7xl p-2 inline-block w-fit mx-auto font-bangers bg-gradient-to-tr from-yellow-400 via-yellow-400 via-30% to-lime-300 text-transparent bg-clip-text drop-shadow-[4px_4px_1px_rgba(0,0,0,.7)]">
-        {title}
-      </h1>
+      <div className="flex flex-col-reverse gap-4 md:flex-row w-fit mx-auto items-center ">
+        <h1 className="text-5xl md:text-7xl p-2 inline-block w-fit mx-auto font-bangers bg-gradient-to-tr from-yellow-400 via-yellow-400 via-30% to-lime-300 text-transparent bg-clip-text drop-shadow-[4px_4px_1px_rgba(0,0,0,.7)]">
+          {title}
+        </h1>
+        {icon && <Image src={getIconUrl()} className="w-20 md:w-24 ml-6" />}
+      </div>
+
       {children && (
-        <p className="text-2xl max-w-[70%] mx-auto mt-2 mb-8 font-sour-gummy text-green-900 leading-8 drop-shadow-md">
+        <p className="text-xl md:text-2xl max-w-[70%] mx-auto mt-2 mb-8 font-sour-gummy text-green-900 leading-8 drop-shadow-md">
           {children}
         </p>
       )}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,8 +6,8 @@ import SectionTitle from '../components/ui/SectionTitle';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-items-center w-full min-h-screen max-w-6xl mx-auto px-6 py-4 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col flex-1 gap-8 items-center">
+    <div className="flex flex-col items-center justify-items-center w-full min-h-screen max-w-6xl mx-auto px-2 md:px-6 py-4 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col flex-1 gap-4 md:gap-8 items-center">
         <Section>
           <SectionTitle title="¿A quien va dirigida?">
             Este evento será un punto de encuentro para profesionales,
@@ -28,7 +28,7 @@ export default function Home() {
         </Section> */}
 
         <Section>
-          <SectionTitle title="Sponsors">
+          <SectionTitle title="Sponsors" icon="thank-you">
             Nuestros patrocinadores hacen posible esta conferencia, ayudándonos
             a inspirar y apoyar a la próxima generación de desarrolladores.
           </SectionTitle>
@@ -37,7 +37,7 @@ export default function Home() {
         </Section>
 
         <Section>
-          <SectionTitle title="Comunidades que apoyan">
+          <SectionTitle title="Comunidades" icon="love">
             Conoce a las comunidades que impulsan el conocimiento, la innovación
             y el networking en el mundo del software apoyando este evento.
           </SectionTitle>
@@ -45,7 +45,7 @@ export default function Home() {
         </Section>
 
         <Section>
-          <SectionTitle title="El equipo">
+          <SectionTitle title="El equipo" icon="pow">
             Conoce a las personas creativas que con conocimiento y dedicación,
             generan el espacio ideal para aprender, conectar y crecer en el
             mundo de la programación.

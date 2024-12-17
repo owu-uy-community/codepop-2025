e5b05d5365a25c3c61099ecc45c2e208908902e1
Santiago Ferreira <san650@gmail.com>
2024-12-17 20:31:33 -0300

    Add github action to publish github pages (#6)
    * Add Github workflow
    * Add CNAME file
    * Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,28 @@ const __dirname = path.dirname(__filename);
 console.log('Dir name: ', __dirname);
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@routes/*': path.resolve(__dirname, './src/routes'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
+//
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  const defaultConfig = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@routes/*': path.resolve(__dirname, './src/routes'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@components': path.resolve(__dirname, './src/components'),
+      },
     },
-  },
-});
+  };
+
+  switch(command) {
+    case "serve":
+      return defaultConfig;
+    case "build":
+      return Object.merge(defaultConfig, {
+        base: '/codepop-2025/'
+      })
+    default:
+      throw `Unknown command: ${command}`;
+  }
+})
